289688dbffa5aee0ec37905e1fa3cafa0159a94c
Thomas <118468472+TomDazz@users.noreply.github.com>
2025-10-25 05:10:50 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -77,6 +77,58 @@ def get_weather(city: str):
     if "error" in data:
         raise HTTPException(status_code=500, detail=data["error"].get("info", "Unknown error"))
 
+    uk_tz = pytz.timezone("Europe/London")
+    timestamp = datetime.now(uk_tz).strftime("%Y-%m-%d %H:%M:%S")
+
+    # Local Northern Irish translations
+    slang_map = {
+        "rain": "Bucketing it down",
+        "showers": "Spitting a bit",
+        "drizzle": "Light drizzle",
+        "cloud": "A bit dull out",
+        "overcast": "Grey",
+        "sun": "Roasting!",
+        "clear": "sky’s clear",
+        "fog": "Ye can’t see yer hand in front of ye",
+        "mist": "bit misty",
+        "snow": "Baltic! There’s snow",
+        "wind": "Blowin’ a gale out there"
+    }
+
+    # Get the weather description and apply local flavour
+    description = data["current"]["weather_descriptions"][0]
+    description_lower = description.lower()
+    local_phrase = "Grand day, so it is!"
+
+    for word, slang in slang_map.items():
+        if word in description_lower:
+            local_phrase = slang
+            break
+
+    return {
+        "datetime": timestamp,
+        "location": data["location"]["name"],
+        "country": data["location"]["country"],
+        "temperature_c": data["current"]["temperature"],
+        "weather_descriptions": [local_phrase],
+        "wind_speed_kmh": data["current"]["wind_speed"],
+        "humidity": data["current"]["humidity"],
+        "feelslike_c": data["current"]["feelslike"]
+    }
+
+#old code
+#@app.get("/weather/{city}", dependencies=[Depends(verify_api_key)])
+#def get_weather(city: str):
+    url = f"http://api.weatherstack.com/current?access_key={API_KEY}&query={city}&units=m"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=500, detail="Error fetching weather data")
+
+    data = response.json()
+    if "error" in data:
+        raise HTTPException(status_code=500, detail=data["error"].get("info", "Unknown error"))
+
     # Get current UK time (BST/GMT aware)
     uk_tz = pytz.timezone("Europe/London")
     timestamp = datetime.now(uk_tz).strftime("%Y-%m-%d %H:%M:%S")
@@ -91,3 +143,5 @@ def get_weather(city: str):
         "humidity": data["current"]["humidity"],
         "feelslike_c": data["current"]["feelslike"]
     }
+
+
